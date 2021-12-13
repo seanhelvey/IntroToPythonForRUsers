b1f68262b161841403b005bef8a5781f255c3dd9
seanhelvey <sean.helvey@gmail.com>
2021-12-13 08:00:13 -0800

    Remove extra key.r and key.py files in root dir. Edit several comments.

--- key.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-
-raw_data = pd.read_csv('ugly_sweaters.csv', index_col='sweater')
-sweaters = raw_data[raw_data['hs_tf'] == 'Yes']
-
-sweaters['colors_list'] = sweaters['colors'].str.split(',')
-color_data = sweaters.explode('colors_list')
-sweaters['num_colors'] = color_data.groupby('sweater')['colors_list'].count()
